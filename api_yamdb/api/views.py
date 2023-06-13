69e2d146bcba0231fe445519673edb96ee253eee
Alexey Chizhov <aalexey-chizhov@yandex.ru>
2023-06-13 13:26:41 +0300

    Добавлены системы регистрации и аутентификации, подтверждения через почту и получение токена

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,11 +1,15 @@
+from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import send_mail
 from django.core.management.utils import get_random_secret_key
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework import status
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework_simplejwt.tokens import AccessToken
 
-from .serializers import SignUpSerializer
+from users.models import User
+from .serializers import SignUpSerializer, TokenSerializer
 
 
 class SignUpView(APIView):
@@ -17,7 +21,7 @@ class SignUpView(APIView):
 
     def post(self, request):
         serializer = SignUpSerializer(data=request.data)
-        serializer.isValid(raise_exception=True)
+        serializer.is_valid(raise_exception=True)
         user = serializer.save()
         confirmation_code = get_random_secret_key()
         user.save()
@@ -28,11 +32,24 @@ class SignUpView(APIView):
             [user.email],
             fail_silently=False,
         )
-        return Response
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 class TokenView(APIView):
     """
     Пользователь отправляет POST-запрос, в ответе на запрос ему приходит token.
     """
-    ...
+    permission_classes = (AllowAny,)
+
+    def post(self, request):
+        serializer = TokenSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        username = serializer.data['username']
+        user = get_object_or_404(User, username=username)
+        confirmation_code = default_token_generator.make_token(user)
+        if not default_token_generator.check_token(user, confirmation_code):
+            return Response(
+                {'confirmation_code': 'Неверный код подтверждения'},
+                status=status.HTTP_400_BAD_REQUEST)
+        token = AccessToken.for_user(user)
+        return Response({'token': str(token)}, status=status.HTTP_200_OK)
